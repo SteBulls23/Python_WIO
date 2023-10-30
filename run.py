98096e9ce1f718bfadff533abb339abfa1e99395
SteBulls23 <132340355+SteBulls23@users.noreply.github.com>
2023-10-30 15:29:26 +0100

    stat

--- run.py
+++ run.py
@@ -2,15 +2,19 @@ from flask import Flask, jsonify, make_response
 from http import HTTPStatus
 from src.app import create_app
 
-app = create_app()
-
-@app.route("/welcome", methods=["GET"]) #@app.get("/welcome")
-def welcome():
-    return "Ciao"
+app = create_app("default")
 
 @app.route("/", methods=["GET"]) #@app.get("/welcome")
 def ciao():
     return [
         {"message" : "Ciao"}, {"message" : "Buongiorno"}, HTTPStatus.CREATED
     ]
+
+@app.route("/welcome", methods=["GET"]) #@app.get("/welcome")
+def welcome():
+    return "welcome"
+
+@app.route("/login", methods=["GET"]) 
+def login():
+    return "login"
 
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from .config import config
 
 #flask application factory function
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     return app
