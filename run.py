ae34db80b3b44b7c08bc682906b85f6236d3a6fb
SteBulls23 <132340355+SteBulls23@users.noreply.github.com>
2023-10-30 16:10:58 +0100

    stat

--- run.py
+++ run.py
@@ -4,17 +4,5 @@ from src.app import create_app
 
 app = create_app("default")
 
-@app.route("/", methods=["GET"]) #@app.get("/welcome")
-def ciao():
-    return [
-        {"message" : "Ciao"}, {"message" : "Buongiorno"}, HTTPStatus.CREATED
-    ]
 
-@app.route("/welcome", methods=["GET"]) #@app.get("/welcome")
-def welcome():
-    return "welcome"
-
-@app.route("/login", methods=["GET"]) 
-def login():
-    return "login"
 
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .config import config
+from src.app.main.views import main
 
 #flask application factory function
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    app.register_blueprint(main, url_prefix = "/main")
     return app
